c611d3ab11e71f436294093ffc3c78bae1d9b834
Keks <keks@htmlacademy.ru>
2025-11-28 12:05:56 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/functions.js
@@ -0,0 +1,42 @@
+// Функция для проверки длины строки
+function checkStringLength(string, maxLength) {
+  return string.length <= maxLength;
+}
+
+// Функция для проверки, является ли строка палиндромом
+function isPalindrome(string) {
+  const normalizedString = string.replaceAll(' ', '').toLowerCase();
+
+  let reversedString = '';
+
+  for (let i = normalizedString.length - 1; i >= 0; i--) {
+    reversedString += normalizedString[i];
+  }
+
+  return normalizedString === reversedString;
+}
+
+// Дополнительная функция
+function extractNumber(string) {
+  const stringValue = string.toString();
+
+  let digitsString = '';
+
+  for (let i = 0; i < stringValue.length; i++) {
+    const char = stringValue[i];
+    const digit = parseInt(char, 10);
+    if (!Number.isNaN(digit)) {
+      digitsString += char;
+    }
+  }
+
+  if (digitsString === '') {
+    return NaN;
+  }
+
+  return parseInt(digitsString, 10);
+}
+
+checkStringLength('hello', 10);
+isPalindrome('топот');
+extractNumber('1234567890');
---
+++ 4/js/main.js
@@ -0,0 +1,102 @@
+const COMMENT_MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+const COMMENT_NAMES = [
+  'Иван', 'Мария', 'Александр', 'Анна', 'Дмитрий', 'Елена',
+  'Сергей', 'Ольга', 'Андрей', 'Наталья', 'Максим', 'Татьяна',
+  'Артём', 'Екатерина', 'Павел'
+];
+
+const PHOTO_DESCRIPTIONS = ['Красивый закат над морем',
+  'Городской пейзаж в вечернее время',
+  'Природа в полном расцвете',
+  'Архитектурное наследие',
+  'Жизнь в движении',
+  'Моменты счастья',
+  'Путешествие по миру',
+  'Уличная фотография',
+  'Портрет в естественном свете',
+  'Абстрактная композиция',
+  'Животные в их естественной среде',
+  'Еда как искусство',
+  'Спортивные достижения',
+  'Музыка и эмоции',
+  'Семейные моменты',
+  'Городская жизнь',
+  'Природные красоты',
+  'Творческий эксперимент',
+  'Эмоциональный портрет',
+  'Архитектурные детали',
+  'Пейзаж в тумане',
+  'Яркие цвета жизни',
+  'Чёрно-белая эстетика',
+  'Макросъёмка',
+  'Широкоугольная перспектива'
+];
+
+let commentIdCounter = 1;
+
+function getRandomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomArrayElement(array) {
+  return array[getRandomInteger(0, array.length - 1)];
+}
+
+function generateComment() {
+  const commentId = commentIdCounter++;
+
+  const messageCount = getRandomInteger(1, 2);
+  let message = '';
+  if (messageCount === 1) {
+    message = getRandomArrayElement(COMMENT_MESSAGES);
+  } else {
+    const firstMessage = getRandomArrayElement(COMMENT_MESSAGES);
+    const secondMessage = getRandomArrayElement(COMMENT_MESSAGES.filter((msg) => msg !== firstMessage));
+    message = `${firstMessage} ${secondMessage}`;
+  }
+
+  return {
+    id: commentId,
+    avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
+    message: message,
+    name: getRandomArrayElement(COMMENT_NAMES)
+  };
+}
+
+function generateComments() {
+  const commentsCount = getRandomInteger(0, 30);
+  const comments = [];
+  for (let i = 0; i < commentsCount; i++) {
+    comments.push(generateComment());
+  }
+  return comments;
+}
+
+function generatePhoto(id) {
+  return {
+    id: id,
+    url: `photos/${id}.jpg`,
+    description: PHOTO_DESCRIPTIONS[id - 1] || `Описание фотографии ${id}`,
+    likes: getRandomInteger(15, 200),
+    comments: generateComments()
+  };
+}
+
+function generatePhotos() {
+  const photosArray = [];
+  for (let i = 1; i <= 25; i++) {
+    photosArray.push(generatePhoto(i));
+  }
+  return photosArray;
+}
+
+const photos = generatePhotos();
+console.log(photos);
