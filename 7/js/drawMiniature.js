2fa59b13b5fdd770978c63acefcb3d9032e42222
Keks <keks@htmlacademy.ru>
2025-12-03 16:22:13 +0300

    :heavy_check_mark: Сборка #7

--- 7/js/drawMiniature.js
+++ 7/js/drawMiniature.js
@@ -1,24 +1,20 @@
-import { generatePhotos } from './data.js';
+export function drawMiniature(photos) {
+  const picturesContainer = document.querySelector('.pictures');
+  const template = document.querySelector('#picture').content;
 
-const picturesContainer = document.querySelector('.pictures');
-const template = document.querySelector('#picture').content;
+  const fragment = document.createDocumentFragment();
 
-const photos = generatePhotos();
+  photos.forEach((photo) => {
+    const el = template.cloneNode(true);
+    const pictureImage = el.querySelector('.picture__img');
+    const pictureComments = el.querySelector('.picture__comments');
+    const pictureLikes = el.querySelector('.picture__likes');
 
-const fragment = document.createDocumentFragment();
-
-photos.forEach((photo) => {
-  const el = template.cloneNode(true);
-  const pictureImage = el.querySelector('.picture__img');
-  const pictureComments = el.querySelector('.picture__comments');
-  const pictureLikes = el.querySelector('.picture__likes');
-
-  pictureImage.src = photo.url;
-  pictureImage.alt = photo.description;
-  pictureComments.textContent = photo.comments;
-  pictureLikes.textContent = photo.likes;
-  fragment.append(el);
-});
-
-picturesContainer.append(fragment);
-
+    pictureImage.src = photo.url;
+    pictureImage.alt = photo.description;
+    pictureComments.textContent = photo.comments;
+    pictureLikes.textContent = photo.likes;
+    fragment.append(el);
+  });
+  picturesContainer.append(fragment);
+}
--- 7/js/main.js
+++ 7/js/main.js
@@ -1,3 +1,5 @@
 import { generatePhotos } from './data.js';
+import { drawMiniature } from './drawMiniature.js';
 
 const photos = generatePhotos();
+drawMiniature(photos);
